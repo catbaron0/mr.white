2d634f83f6542e7e2c352afb9b5a21d9215bd2a1
catbaron <catbaron@live.cn>
2025-07-05 16:15:45 +0900

    refactors

--- workers/translator.py
+++ workers/translator.py
@@ -35,14 +35,19 @@ class Translator:
         author_is_bot = message.author.bot
 
         if channel_id not in self.auto_trans_channel:
+            logger.info(f"skip channel: {channel_id}")
             return  # 忽略非自动翻译频道的消息
         if not text:
+            logger.info("skip empty message")
             return  # 忽略空消息
         if author_is_bot and author_id not in self.auto_trans_channel[channel_id]["allow_bots"]:
+            logger.info(f"skip bot message: {author_id} in channel {channel_id}")
             return  # 忽略机器人消息
+
         res = await gpt_translate_to_zh(text)
         if res != text:
             try:
+                logger.info(f"message translated: \n{text}\n{res}")
                 await message.reply(res)
             except Exception as e:
                 logger.error(f"自动翻译失败: {e}")
